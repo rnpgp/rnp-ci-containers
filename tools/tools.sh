ddb4eeda7ef45f857e02f7cf3315e864f782be92
Maxim [maxirmx] Samsonov <m.samsonov@computer.org>
2023-09-08 12:55:17 +0300

    Fixed Python installation

--- tools/tools.sh
+++ tools/tools.sh
@@ -31,6 +31,20 @@ run_in_python_venv() {
   )
 }
 
+# If target does not exist, create symlink from source to target.
+ensure_symlink_to_target() {
+  local from="${1:?Missing source}"
+  local to="${2:?Missing target}"
+
+  if [[ -e "${from}" && ! -e "${to}" ]]; then
+    if ! sudo ln -s "${from}" "${to}"
+    then
+      >&2 echo "Error: ${to} still not available after symlink.  Aborting."
+      exit 1
+    fi
+  fi
+}
+
 install_cmake() {
   echo "Running install_cmake version ${CMAKE_VERSION} for ${ARCH}"
   local cmake_install=${LOCAL_BUILDS}/cmake
@@ -43,6 +57,7 @@ install_cmake() {
 }
 
 build_and_install_python() {
+  echo "Running build_and_install_python version ${PYTHON_VERSION}"
   local python_build=${LOCAL_BUILDS}/python
   mkdir -p "${python_build}"
   pushd "${python_build}"
@@ -50,9 +65,9 @@ build_and_install_python() {
   tar -xf python.tar.xz --strip 1
   ./configure --enable-optimizations --prefix=/usr
   make -j"${MAKE_PARALLEL}" && sudo make install
-  ensure_symlink_to_target /usr/bin/python3 /usr/bin/python
   popd
   rm -rf "${python_build}"
+  ensure_symlink_to_target /usr/bin/python3 /usr/local/bin/python
 }
 
 build_and_install_automake() {
